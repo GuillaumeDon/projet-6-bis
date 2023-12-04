2b5047b9caf452a7d1e4be1fb94c532bd2984ea0
Guillaume Donizetti <glm.donizetti@gmail.com>
2023-12-05 00:19:57 +0100

    mise en place likes et titles

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -7,19 +7,64 @@ class MediaFactory {
         }
     }
 
+    // createImage(media) {
+    //     const img = document.createElement('img');
+    //     img.src = `assets/medias/${media.photographerId}/${media.image}`;
+    //     img.alt = media.title;
+   
+    //     return img;
+    // }
+
+    // createVideo(media) {
+    //     const video = document.createElement('video');
+    //     video.src = `assets/medias/${media.photographerId}/${media.video}`;
+    //     video.setAttribute('controls', '');
+    //     return video;
+    // }
+
+
     createImage(media) {
+        const container = document.createElement('div');
+        container.classList.add('media-container');
+    
         const img = document.createElement('img');
         img.src = `assets/medias/${media.photographerId}/${media.image}`;
         img.alt = media.title;
-   
-        return img;
+        container.appendChild(img);
+    
+        const title = document.createElement('p');
+        title.textContent = media.title;
+        container.appendChild(title);
+    
+        // Assuming media.likes holds the number of likes
+        const likes = document.createElement('span');
+        likes.textContent = `${media.likes} likes`;
+        container.appendChild(likes);
+    
+        return container;
     }
 
     createVideo(media) {
+        const container = document.createElement('div');
+        container.classList.add('media-container');
+    
         const video = document.createElement('video');
         video.src = `assets/medias/${media.photographerId}/${media.video}`;
+        video.alt = media.title;
         video.setAttribute('controls', '');
-        return video;
+        
+        container.appendChild(video);
+    
+        const title = document.createElement('p');
+        title.textContent = media.title;
+        container.appendChild(title);
+    
+        // Assuming media.likes holds the number of likes
+        const likes = document.createElement('span');
+        likes.textContent = `${media.likes} likes`;
+        container.appendChild(likes);
+    
+        return container;
     }
 }
 
