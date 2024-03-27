9c96118daace53e0987ab66181b47247bef95632
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-03-27 22:01:38 +0100

    ouverture modale au clavier

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -60,6 +60,10 @@ document.querySelector("#contact_modal form").addEventListener("submit", functio
 
 // Fermez la modale avec la touche "échap".
 window.addEventListener("keydown", function (event) {
+
+	if (event.key === "Enter") {
+		displayModal();
+	}
 	if (event.key === "Escape") {
 		closeModal();
 	}
