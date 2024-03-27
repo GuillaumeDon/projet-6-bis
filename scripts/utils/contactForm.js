391aa8c3cce59de827c55dc319aa4fa62796f40e
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-03-27 21:41:48 +0100

    remise en place modale

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,36 +1,36 @@
 
 
 
-// function displayModal() {
-// 	const modal = document.getElementById("contact_modal");
-// 	modal.style.display = "block";
-// 	modal.setAttribute("aria-hidden", "false");
+function displayModal() {
+	const modal = document.getElementById("contact_modal");
+	modal.style.display = "block";
+	modal.setAttribute("aria-hidden", "false");
 
-// 	// Focus initial sur le premier champ du formulaire
-// 	const firstInput = document.getElementById("first-name");
-// 	firstInput.focus();
+	// Focus initial sur le premier champ du formulaire
+	const firstInput = document.getElementById("first-name");
+	firstInput.focus();
 
-// 	// Ajoutez la gestion de la navigation clavier entre les inputs
-// 	const formInputs = modal.querySelectorAll("input");
-// 	formInputs.forEach((input, index) => {
-// 		input.addEventListener("keydown", (event) => {
-// 			if (event.key === "ArrowUp" && index > 0) {
-// 				formInputs[index - 1].focus();
-// 			} else if (event.key === "ArrowDown" && index < formInputs.length - 1) {
-// 				formInputs[index + 1].focus();
-// 			}
-// 		});
-// 	});
+	// Ajoutez la gestion de la navigation clavier entre les inputs
+	const formInputs = modal.querySelectorAll("input");
+	formInputs.forEach((input, index) => {
+		input.addEventListener("keydown", (event) => {
+			if (event.key === "ArrowUp" && index > 0) {
+				formInputs[index - 1].focus();
+			} else if (event.key === "ArrowDown" && index < formInputs.length - 1) {
+				formInputs[index + 1].focus();
+			}
+		});
+	});
 
-// 	// Ajoutez la gestion de la validation du formulaire avec la touche "Entrée" sur le bouton de soumission
-// 	const submitButton = modal.querySelector("button[type=\"submit\"]");
-// 	submitButton.addEventListener("keydown", (event) => {
-// 		if (event.key === "Enter") {
-// 			event.preventDefault(); // Empêchez le comportement par défaut du bouton submit
-// 			closeModal(); // Fermez la modale après la validation
-// 		}
-// 	});
-// }
+	// Ajoutez la gestion de la validation du formulaire avec la touche "Entrée" sur le bouton de soumission
+	const submitButton = modal.querySelector("button[type=\"submit\"]");
+	submitButton.addEventListener("keydown", (event) => {
+		if (event.key === "Enter") {
+			event.preventDefault(); // Empêchez le comportement par défaut du bouton submit
+			closeModal(); // Fermez la modale après la validation
+		}
+	});
+}
 
 function closeModal() {
 	const modal = document.getElementById("contact_modal");
